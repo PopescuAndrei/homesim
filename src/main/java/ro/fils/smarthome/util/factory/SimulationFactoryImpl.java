dfa8a14a668fa0864798e3ef3bc53fb70fee9ec0
Popescu Andrei <Popescu Andrei>
2016-03-29 01:37:38 +0300

    merging again

---
+++ src/main/java/ro/fils/smarthome/util/factory/SimulationFactoryImpl.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.fils.smarthome.util.factory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.parser.ParseException;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ro.fils.smarthome.model.Agent;
+import ro.fils.smarthome.sensor.Sensor;
+import ro.fils.smarthome.sensor.SensorReader;
+import ro.fils.smarthome.tasksManagement.TaskReader;
+import ro.fils.smarthome.view.SimulationDisplay;
+
+/**
+ *
+ * @author andre
+ */
+public class SimulationFactoryImpl implements SimulationFactory {
+
+    ClassPathXmlApplicationContext ctx;
+    private ArrayList<Agent> people;
+    private TaskReader taskReader;
+    private Collection<Sensor> sensors;
+    private int days;
+
+    public SimulationFactoryImpl(ClassPathXmlApplicationContext ctx) {
+        this.ctx = ctx;
+        try {
+            taskReader = new TaskReader();
+            people = new ArrayList<>();
+        } catch (ParseException | IOException ex) {
+            Logger.getLogger(SimulationFactoryImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public SimulationDisplay getFactory(String simulationName) {
+//        if(simulationName.equalsIgnoreCase("Old Man")){
+              setElementsForScenarioOldMan();
+//        }else{
+              setElementsForScenarioBoyAndGirl();
+//        }
+//        return new SimulationDisplay(ctx,sensors,people, taskReader.getNeeds()));
+          return null;
+    }
+
+    protected void setElementsForScenarioOldMan() {
+        try {
+            sensors = new SensorReader("/sensors.json").getSensors();
+            //simulationMap = new SimulationMap(context, "/environment.jpg", 50, 2L, people, 43, sensors);
+            //people.add(new Agent("Person1", "/agent.png", 0, simulationMap.getStartingPoint(), taskReader.getNeeds("old_man_needs.json")));
+            //people.add(new Agent("Boy", "/boy_avatar_48p.png", 0, simulationMap.getStartingPoint(), taskReader.getNeeds("/boy_needs.json")));
+            this.days = 30;
+        } catch (Exception ex) {
+            Logger.getLogger(SimulationFactoryImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    protected void setElementsForScenarioBoyAndGirl(){
+        try {
+            sensors = new SensorReader("/sensors2.json").getSensors();
+            //simulationMap = new SimulationMap(context, "/environment.jpg", 50, 2L, people, 43, sensors);
+            //people.add(new Agent("Person1", "/agent.png", 0, simulationMap.getStartingPoint(), taskReader.getNeeds("old_man_needs.json")));
+            //people.add(new Agent("Boy", "/boy_avatar_48p.png", 0, simulationMap.getStartingPoint(), taskReader.getNeeds("/boy_needs.json")));
+            this.days = 30;
+        } catch (Exception ex) {
+            Logger.getLogger(SimulationFactoryImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
--- src/main/java/ro/fils/smarthome/view/SimulatorFrame.java
+++ src/main/java/ro/fils/smarthome/view/SimulatorFrame.java
@@ -57,7 +57,6 @@ public class SimulatorFrame extends javax.swing.JFrame {
         this.context = ctx;
         initSimulatorTools();
         initComponents();
-        setExtendedState(Frame.MAXIMIZED_BOTH);
         setTitle("Simulator");
     }
 
@@ -392,41 +391,6 @@ public class SimulatorFrame extends javax.swing.JFrame {
         jPanel2.add(populateAgentsAndNeeds(selectedValue));
         jPanel2.repaint();
     }//GEN-LAST:event_jList2MouseClicked
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SimulatorFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SimulatorFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SimulatorFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SimulatorFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new SimulatorFrame(new ClassPathXmlApplicationContext("classpath:applicationContext.xml")).setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
