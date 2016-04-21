ffadb8de61518e2269c82161db495a68f952f451
AndreiPopescu <andrei.popescu93@gmail.com>
2016-04-21 20:35:03 +0300

    incerc sa vad ceva

--- src/main/java/ro/fils/smarthome/Application.java
+++ src/main/java/ro/fils/smarthome/Application.java
@@ -5,6 +5,7 @@ import ro.fils.smarthome.view.StartFrame;
 public class Application{
 
     public static void main(String[] args) {
+        System.out.println("Vreau doar sa vad daca mai pot face commit");
         StartFrame frame = new StartFrame();
         frame.setVisible(true);
     }
